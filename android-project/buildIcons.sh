52abf50bb9bd77c1c65f7295774cb948c4bb5fa0
Sam Larison <samlarison@gmail.com>
2019-03-25 16:27:43 -0500

    some icons for basic mode

---
+++ android-project/buildIcons.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -f ../icons/icon72.png app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp -f ../icons/icon48.png app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp -f ../icons/icon96.png app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp -f ../icons/icon144.png app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+cp -f ../icons/icon192.png app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
---
+++ android-project/buildIconsBasic.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -f ../icons/basic-with-alpha/icon72.png app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp -f ../icons/basic-with-alpha/icon48.png app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp -f ../icons/basic-with-alpha/icon96.png app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp -f ../icons/basic-with-alpha/icon144.png app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+cp -f ../icons/basic-with-alpha/icon192.png app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
